5bed9f0a935aa3f23aba320f8c4b04c57e889776
Simon Wicky <simon.wicky@epfl.ch>
2020-01-31 10:08:36 +0100

    remove useless comments

--- utils/SecretSharer.go
+++ utils/SecretSharer.go
@@ -6,55 +6,7 @@ import (
 	"os"
 )
 
-// type SecretSharer struct {
-// 	cloves map[*big.Int] []*utils.Cloves
-// }
 
-// func NewSecretSharer() *SecretSharer {
-// 	return &SecretSharer{
-// 		cloves: make(map[*big.Int][]*utils.Cloves),
-// 	}
-// }
-
-// func (s *SecretSharer) checkSecret(id *big.Int) {
-// 	array := s.cloves[id]
-// 	k := array[0].K
-// 	data_len := len(array[0].Data)
-// 	sequence_numbers := make(map[int]struct{})
-// 	for _, c := range array {
-// 		//check k
-// 		if c.K != k {
-// 			fmt.Fprintln(os.Stderr,"Different K for same Id, dropping secret")
-// 			s.cloves[id] = nil
-// 			return
-// 		}
-// 		//check data_len
-// 		if len(c.Data) != data_len {
-// 			fmt.Fprintln(os.Stderr,"Different len for same Id, dropping secret")
-// 			s.cloves[id] = nil
-// 			return
-// 		}
-
-// 		sequence_numbers[c.Sequence_number] = struct{}{}
-// 	}
-
-// 	n := len(array)
-// 	if n < k {
-// 		fmt.Fprintln(os.Stderr,"Not enough clove to decrypt secret yet")
-// 		return
-// 	}
-
-// 	//sequence_numbers check
-// 	var sequence_numbers_array []int
-// 	for k := range sequence_numbers {
-// 		sequence_numbers_array = append(sequence_numbers_array, k)
-// 	}
-// 	if len(sequence_numbers_array) < k {
-// 		fmt.Fprintln(os.Stderr,"Not enough sequence number to decrypt secret yet")
-// 		return
-// 	}
-// 	s.recoverSecret(array)
-// }
 
 func recoverSecret(data [][]byte, pointX []int) ([]byte, error) {
 	/*if len(data) != len(pointX) {
@@ -75,7 +27,6 @@ func recoverSecret(data [][]byte, pointX []int) ([]byte, error) {
 	}
 	return CombineAndDecrypt(result_data)
 
-	//direct data to proxy/content/whatever
 }
 
 //==============================
